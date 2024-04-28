b4ca0eae341a39f8d48e9edb1445fd44c60c241c
shan <133497750+shan1311@users.noreply.github.com>
2024-04-28 13:05:40 +0530

    Update EmployeeLogin.js

--- src/components/employee/EmployeeLogin.js
+++ src/components/employee/EmployeeLogin.js
@@ -6,7 +6,7 @@ import logoImage from '../assets/whatsapp-image-20240123-at-1705-1@2x.png';
 import { AppBar, Toolbar, Container } from "@mui/material";
 import styled from 'styled-components';
 
-import BackgroundImage from '../../assets/1.jpg'
+import BackgroundImage from '../assets/1.jpg'
 const AnimatedBackground = styled.div`
 		min-height: 100vh;background: linear-gradient(to bottom, rgba(78, 101, 255, 0.8), rgba(146, 239, 253, 0.8)), url(${BackgroundImage}) no-repeat center center fixed;background-size: cover;`;
 
